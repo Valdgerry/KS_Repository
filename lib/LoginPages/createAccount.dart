4848475648356e1effbf2d4784c6dab5f55638cc
Brandondgb <brandondegbe95@gmail.com>
2025-04-03 11:32:34 +0100

    Start

---
+++ lib/LoginPages/createAccount.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class createAccount extends StatelessWidget {
+  const createAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        children: [
+          Image(image: AssetImage("assets/Group.png")),
+          Text("data")
+        ],
+      ),
+      
+    );
+  }
+}
